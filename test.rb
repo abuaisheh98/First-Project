5dd22849a93f4239ff95d49d20e18cd6f0a71bce
abuaisheh24 <labuaisheh@everconnect.com>
2022-11-02 09:43:34 +0200

    ATM machine assignment

--- test.rb
+++ test.rb
@@ -1,11 +1,55 @@
+saved_password=12345
+amount=1000
+
+puts "Please Enter Your Password"
+password_enterd=gets
+password_as_a_number=password_enterd.to_i
+
+count=0
+
+for i in 0..2
+	count+=1
+
+	if password_as_a_number == saved_password
+		puts "How much money do you want ?"
+		taked_money=gets
+		taked_money_as_a_number=taked_money.to_i
+
+		for x in 0..2
+			count+=1
+			if taked_money_as_a_number <= 0 or taked_money_as_a_number>amount
+
+				if count !=3
+					puts "Oops,The amount you entered is not allowed .."
+					taked_money=gets
+					taked_money_as_a_number=taked_money.to_i
+					
+				end
+
+			else
+				puts "Your total amount is #{amount-taked_money_as_a_number} and withdrawn amount is #{taked_money_as_a_number}"
+				break
+			end
+
+		end
+
+		break
 
 
-puts "hi Laith"
+	else
 
-x=10
-y=25
-puts x+y 
+		if count != 3
+			puts "Please try again ..."
+			password_enterd=gets
+			password_as_a_number=password_enterd.to_i
+			
+		else
+			puts "Sorry , try again after 1 houre"
+		end
 
+	end
+
+end
 
 
 
