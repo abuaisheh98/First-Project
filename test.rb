f60cc3ba20e31e53d10847c27e883d0db85bf67e
abuaisheh24 <labuaisheh@everconnect.com>
2022-11-02 09:48:30 +0200

    Add comments to make the code readable

--- test.rb
+++ test.rb
@@ -5,18 +5,22 @@ puts "Please Enter Your Password"
 password_enterd=gets
 password_as_a_number=password_enterd.to_i
 
+#count var for validation
 count=0
 
 for i in 0..2
 	count+=1
 
+	#check password
 	if password_as_a_number == saved_password
 		puts "How much money do you want ?"
 		taked_money=gets
 		taked_money_as_a_number=taked_money.to_i
 
+		#loop for check the user amount
 		for x in 0..2
 			count+=1
+
 			if taked_money_as_a_number <= 0 or taked_money_as_a_number>amount
 
 				if count !=3
