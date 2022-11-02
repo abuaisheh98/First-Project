ec944b83ea556da33d5e7a7f473a451091a4bdde
abuaisheh24 <labuaisheh@everconnect.com>
2022-11-02 15:13:20 +0200

    Practicing loops and conditional statement

---
+++ condition.rb
@@ -0,0 +1,75 @@
+# my_number=65
+# guess_number=0
+# puts "Predict a number between 0 and 100 ..."
+
+# while guess_number != my_number
+#  guess_number=gets.to_i
+#  if guess_number > my_number
+#    puts "your number is high .. try again"
+#    next
+
+#  elsif guess_number < my_number
+#    puts "your number is low .. try again"
+#   next
+#  end
+
+# end
+
+
+
+# my_number=65
+# guess_number=0
+# puts "Predict a number between 0 and 100 ..."
+
+# until guess_number == my_number
+#   guess_number=gets.to_i
+#   if guess_number > my_number
+#     puts "your number is high .. try again"
+#     next
+
+#   elsif guess_number < my_number
+#     puts "your number is low .. try again"
+#     next
+#   end  
+# end
+
+
+
+
+# my_number=65
+# guess_number=0
+# puts "Predict a number between 0 and 100 ..."
+
+# loop do
+#   guess_number=gets.to_i
+
+#   if guess_number==my_number
+#     puts "you are win"
+#     break
+#   else
+#     puts (guess_number>my_number)? "your number is high .. try again" : "your number is low .. try again"
+#   end
+
+# end
+
+
+
+
+
+# my_number=65
+# guess_number=0
+# puts "Predict a number between 0 and 100 ..."
+
+# loop do 
+#   guess_number=gets.to_i
+
+#   unless guess_number == my_number
+#     puts (guess_number>my_number)? "your number is high .. try again" : "your number is low .. try again"
+#   else
+#     puts "you are win"
+#     break
+#   end
+# end
+
+
+
